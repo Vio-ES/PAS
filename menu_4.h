56f2712d8a335eb97a10594773f663c0b65b9730
Adiva Veronia <85977959+adivaveronia@users.noreply.github.com>
2021-06-22 23:12:36 +0700

    Update menu_4.h

--- menu_4.h
+++ menu_4.h
@@ -19,37 +19,37 @@ void evaluasi (long int pemasukan, long int totalPengeluaran, long int rencanaTo
 	FILE *out = fopen("Pemasukan.txt","r");
 	if (out == NULL){
         printf("isi File Kosong!");
-    }
-	fscanf(out, "%[^\n]", tanggal); // membaca baris pertama sebagai kesatuan
-	fscanf(out, "%s %ld", pemasukan1, &pemasukan); // membaca baris kedua, nominal menjadi ke variabel pemasukan
-	
-	sisaUang = pemasukan - totalPengeluaran; // menghitung sisa dari pemasukan
-	// menghitung selisih rencana pengeluaran dgn pengeluaran sebenarnya
-	selisihPengeluaran = rencanaTotalPengeluaran - totalPengeluaran;
-	
-	if (sisaUang < 0) { // sisa uang menjadi pengeluaran lebih
-		pengeluaranLebih = abs(sisaUang); 
-		sisaUang = 0;
-	}
-	// jika selisih pengeluaran hasilnya negatif
-	if (selisihPengeluaran < 0) selisihPengeluaran = abs(selisihPengeluaran);
-	
-	printf("\n\n--EVALUASI KEUANGAN--\n");
-	printf("Pemasukan                 = Rp%ld,00\n", pemasukan); // banyak pemasukan
-	printf("Total Rencana Pengeluaran = Rp%ld,00\n", rencanaTotalPengeluaran); // total pengeluaran seusai perencanaan
-	printf("Total Pengeluaran         = Rp%ld,00\n", totalPengeluaran); // total pengeluaran sebenarnya
-	// selisih rencana pengeluaran dgn pengeluaran sebenarnya
-	if(totalPengeluaran > rencanaTotalPengeluaran) printf("Selisih Pengeluaran       = Lebih Rp%ld,00 dari perencanaan\n", selisihPengeluaran);
-	else if(totalPengeluaran < rencanaTotalPengeluaran) printf("Selisih Pengeluaran       = Kurang Rp%ld,00 dari perencanaan\n", selisihPengeluaran);
-	else printf("Selisih Pengeluaran       = Rp%ld,00\n", selisihPengeluaran); 
-	printf("Sisa dari Pemasukan       = Rp%ld,00\n", sisaUang); // sisa pemasukan - pengeluaran sebenarnya
-	printf("Kelebihan Pengeluaran     = Rp%ld,00\n", pengeluaranLebih);
-	printf("\n");
-		
-    if (sisaUang > 0) printf("Selamat anda telah menghemat uang anda!\n");
-    if (sisaUang == 0 && pengeluaranLebih == 0) printf("Selamat anda telah mengoptimalkan keuangan anda!\n");
-    if (pengeluaranLebih > 0) printf("Let's do better next time!\n");
-    
-    fclose(out);
-}
+    }else{
+		fscanf(out, "%[^\n]", tanggal); // membaca baris pertama sebagai kesatuan
+		fscanf(out, "%s %ld", pemasukan1, &pemasukan); // membaca baris kedua, nominal menjadi ke variabel pemasukan
 
+		sisaUang = pemasukan - totalPengeluaran; // menghitung sisa dari pemasukan
+		// menghitung selisih rencana pengeluaran dgn pengeluaran sebenarnya
+		selisihPengeluaran = rencanaTotalPengeluaran - totalPengeluaran;
+
+		if (sisaUang < 0) { // sisa uang menjadi pengeluaran lebih
+			pengeluaranLebih = abs(sisaUang); 
+			sisaUang = 0;
+		}
+		// jika selisih pengeluaran hasilnya negatif
+		if (selisihPengeluaran < 0) selisihPengeluaran = abs(selisihPengeluaran);
+
+		printf("\n\n--EVALUASI KEUANGAN--\n");
+		printf("Pemasukan                 = Rp%ld,00\n", pemasukan); // banyak pemasukan
+		printf("Total Rencana Pengeluaran = Rp%ld,00\n", rencanaTotalPengeluaran); // total pengeluaran seusai perencanaan
+		printf("Total Pengeluaran         = Rp%ld,00\n", totalPengeluaran); // total pengeluaran sebenarnya
+		// selisih rencana pengeluaran dgn pengeluaran sebenarnya
+		if(totalPengeluaran > rencanaTotalPengeluaran) printf("Selisih Pengeluaran       = Lebih Rp%ld,00 dari perencanaan\n", selisihPengeluaran);
+		else if(totalPengeluaran < rencanaTotalPengeluaran) printf("Selisih Pengeluaran       = Kurang Rp%ld,00 dari perencanaan\n", selisihPengeluaran);
+		else printf("Selisih Pengeluaran       = Rp%ld,00\n", selisihPengeluaran); 
+		printf("Sisa dari Pemasukan       = Rp%ld,00\n", sisaUang); // sisa pemasukan - pengeluaran sebenarnya
+		printf("Kelebihan Pengeluaran     = Rp%ld,00\n", pengeluaranLebih);
+		printf("\n");
+
+	    if (sisaUang > 0) printf("Selamat anda telah menghemat uang anda!\n");
+	    if (sisaUang == 0 && pengeluaranLebih == 0) printf("Selamat anda telah mengoptimalkan keuangan anda!\n");
+	    if (pengeluaranLebih > 0) printf("Let's do better next time!\n");
+
+	    fclose(out);
+	}
+}
