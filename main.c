ea36205236c7fa8d65c823e75812315fb7e60562
Adiva Veronia <85977959+adivaveronia@users.noreply.github.com>
2021-06-23 20:05:34 +0700

    Update main.c

--- main.c
+++ main.c
@@ -25,51 +25,52 @@
 int main(void) {
     int menu;
     long int pemasukan, totalPengeluaran = 0, rencanaTotalPengeluaran = 0;
-    char menuKeluar;
+    int menuKeluar;
     do{
-      printf("Selamat datang di Program Manajemen Keuangan!\n");
-      printf("Terdapat 5 menu yang tersedia :\n1.  Pemasukan : jumlah pemasukan yang akan digunakan untuk pengeluaran\n2. Rencana Pemasukan : rincian perencanaan pengeluaran yang akan digunakan\n3. Catatan Pengeluaran : rincian pengeluaran yang sebenarnya (bisa berbeda dengan rencana pengeluaran)\n4. Evaluasi\n5. Exit\n");
-        do{
-            printf("Silahkan ketik menu yang anda inginkan\n");
-            scanf("%d", &menu);
-            switch (menu){
-              case 1:
-                  menu_1();
-              case 2:
-                  if (error_handling("Pemasukan.txt", 1) == 0){
-			  			        break;
-					        }	
-                  menu_2();
-              case 3:
-                  if (error_handling("Pemasukan.txt", 1) == 0){
-			  			        break;
-				        	}
-				        	if (error_handling("rencana pengeluaran.txt", 2) == 0){
-			        		  	break;
-			        		}	
-                  menu_3(&totalPengeluaran);
-              case 4:
-                  if (error_handling("Pemasukan.txt", 1) == 0){
-			  			        break;
-	        				}
-					        if (error_handling("rencana pengeluaran.txt", 2) == 0){
-			  		      	  break;
-				        	}
-					        if (error_handling("Catatan Pengeluaran.txt", 3) == 0){
-			  		        	    break;
-				        	}	
-                  evaluasi(totalPengeluaran, rencanaTotalPengeluaran);
-                  printf("\nApakah anda ingin mengulang program?\nJika iya tekan Y atau y\nJika tidak tekan N atau n\n");
-                  scanf("%c", &menuKeluar);
-                  if(menuKeluar == 'N' || menuKeluar == 'n')
-                    break;
-              case 5: 
-                  break;
-              default:
-                  printf("Angka yang anda masukkan salah, silahkan input kembali\n");
-                  scanf("%d", &menu);
-            }
-        }while( 1<= menu && menu<= 4);
-    }while(menuKeluar == 'Y' || menuKeluar == 'y');
-    return 0;
+		printf("Selamat datang di Program Manajemen Keuangan!\n");
+		printf("Terdapat 5 menu yang tersedia :\n1. Pemasukan : jumlah pemasukan yang akan digunakan untuk pengeluaran\n2. Rencana Pemasukan : rincian perencanaan pengeluaran yang akan digunakan\n3. Catatan Pengeluaran : rincian pengeluaran yang sebenarnya (bisa berbeda dengan rencana pengeluaran)\n4. Evaluasi\n5. Exit\n");
+		do{
+		    printf("Silahkan ketik menu yang anda inginkan\n");
+		    scanf("%d", &menu);
+		    switch (menu){
+				case 1:
+					menu_1();
+				case 2:
+					if (error_handling("Pemasukan.txt", 1) == 0){
+						break;
+					}	
+					printf("Silahkan masukkan perencanaan anda : \n");
+					menu_2(&rencanaTotalPengeluaran);
+				case 3:
+				    if (error_handling("Pemasukan.txt", 1) == 0){
+						break;
+				    }
+		        	if (error_handling("rencana pengeluaran.txt", 2) == 0){
+		    			break;
+		    		}	
+				    menu_3(&totalPengeluaran);
+				case 4:
+				    if (error_handling("Pemasukan.txt", 1) == 0){
+				        break;
+					}
+			        if (error_handling("rencana pengeluaran.txt", 2) == 0){
+			      	    break;
+		        	}
+			        if (error_handling("Catatan Pengeluaran.txt", 3) == 0){
+			        	break;
+					}	
+					evaluasi(totalPengeluaran, rencanaTotalPengeluaran);
+					break;					
+				case 5: 
+					break;
+				default:
+					printf("Angka yang anda masukkan salah, silahkan input kembali\n");
+					scanf("%d", &menu);
+		    }
+		    printf("\nApakah anda ingin mengulang program?\n1. Iya\n2. Tidak\n");
+			scanf("%d", &menuKeluar);
+			if(menuKeluar == 2) menu = 6;
+		}while( 1<= menu && menu<= 4);
+	}while(menuKeluar == 1);
+	return 0;
 }
