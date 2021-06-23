a0c4280c0ab52a254ad75f4d04878954cdf1f7a8
ivansridana <85986002+ivansridana@users.noreply.github.com>
2021-06-23 22:19:01 +0800

    Update menu_1.h

--- menu_1.h
+++ menu_1.h
@@ -7,11 +7,11 @@
 void menu_1(){
 	char tanggal[100];
 	long int pemasukan;
-	printf("Masukkan tanggal : "); scanf(" %[^\n]s", tanggal); fflush(stdin);
+	printf("Masukkan tanggal : "); scanf(" %[^\n]s", tanggal); fflush(stdin);  //baris ke-10 dan ke-11 digunakan untuk menginput tanggal dan pemasukan
 	printf("Masukkan pemasukan : "); scanf("%ld", &pemasukan); fflush(stdin);
 
-	FILE *out = fopen("Pemasukan.txt","w");
-	if (out == NULL){
+	FILE *out = fopen("Pemasukan.txt","w");  //modul file handling
+	if (out == NULL){                        //di baris ke-13 hingga ke-18 berisi fungsi untuk mengexport input tadi menjadi sebuah file berupa .txt
         printf("isi File Kosong!");
     }else printf ("Sukses Menambah Data!");
 	fprintf(out,"Tanggal: %s \nPemasukan: %ld\n", tanggal, pemasukan);
